feee0a0f47a5afd4c329454cafc11eb43ae6fa53
JamesTheGeek <radjame52004@gmail.com>
2025-03-02 12:35:38 -0500

    Working on navbar

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express, {Request, Response} from "express";
+import path from "path";
 
 const app = express();
 
+const PORT = 3000
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, world!")
+    res.sendFile(path.join(__dirname, "../static/index.html"));
 });
+
+app.listen(PORT)
+
+console.log("listening on port " + 3000)
